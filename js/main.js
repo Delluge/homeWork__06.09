69dc906a67458823311cef5f4cbbe856010d3e30
Delluge <133694985+Delluge@users.noreply.github.com>
2023-09-06 09:26:48 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+for (let i = 2; i <= 12; i = i + 2) {
+    console.log(`${i}`)
+}
+
+for (let r = 10; r != 0; r = r - 1) {
+    console.log(`${r}`)
+}
+
+let i = 2
+while(i <= 12) {
+    console.log(`${i}`)
+    i = i + 2
+}
+
+let r = 10
+while(r != 0) {
+    console.log(`${r}`)
+    r = r - 1
+}
+
